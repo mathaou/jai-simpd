a5783599b9e7b4803ede8461e2f39cd569eadd3c
Matthew Farstad <matt.farstad@outlook.com>
2023-12-12 15:31:20 -0500

    update

--- tests/build.sh
+++ tests/build.sh
@@ -3,9 +3,9 @@
 rm -rf bin
 mkdir -p bin
 
-jai add_test.jai -output_path bin -exe add -release
-jai subtract_test.jai -output_path bin -exe sub -release
-jai divide_test.jai -output_path bin -exe div -release
-jai multiply_test.jai -output_path bin -exe mult -release
-jai logical_test.jai -output_path bin -exe logic -release
-jai convert_test.jai -output_path bin -exe logic -release
+# jai add_test.jai -output_path bin -exe add -release
+# jai subtract_test.jai -output_path bin -exe sub -release
+# jai divide_test.jai -output_path bin -exe div -release
+# jai multiply_test.jai -output_path bin -exe mult -release
+# jai logical_test.jai -output_path bin -exe logic -release
+jai convert_test.jai -output_path bin -exe convert -release
